6fda73b4bcec176a7278fdd7603327a7d1a83996
Karla C. Ferreira <karlinharural@gmail.com>
2024-03-12 06:20:37 -0300

    Add files via upload

--- src/screens/Calendario/Style.tsx
+++ src/screens/Calendario/Style.tsx
@@ -12,29 +12,6 @@ const Style = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    calendar:{
-        borderRadius: 30,
-        height: '73%',
-        top: '20%',
-    },
-    button:{
-        backgroundColor: 'rgba(84, 42, 12, 0.75)',
-        height: 50,
-        paddingLeft: 20,
-        paddingRight: 10, 
-        borderRadius: 10,
-        margin: 40,
-        marginLeft: 'auto', 
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      text:{
-        color: '#fff',
-        textAlign: 'center',
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginEnd: 20,
-      },
 });
 
 export default Style;
